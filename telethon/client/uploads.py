240e4146abc411abb76bb3e88f8cbb43290be609
s1xu <s1xu@foxmail.com>
2025-02-08 21:53:20 +0800

    fix: support batch sending of image URLs and video URLs in albums

--- telethon/client/uploads.py
+++ telethon/client/uploads.py
@@ -528,7 +528,7 @@ class UploadMethods:
                 ))
 
                 fm = utils.get_input_media(r.photo)
-            elif isinstance(fm, types.InputMediaUploadedDocument):
+            elif isinstance(fm, (types.InputMediaUploadedDocument, types.InputMediaDocumentExternal)):
                 r = await self(functions.messages.UploadMediaRequest(
                     entity, media=fm
                 ))
